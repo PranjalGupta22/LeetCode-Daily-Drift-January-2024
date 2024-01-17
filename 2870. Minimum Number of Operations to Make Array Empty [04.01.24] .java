7158116f77c6d094f1efcb756ad53787a121306f
Pranjal Gupta <123003273+PranjalGupta22@users.noreply.github.com>
2024-01-17 19:58:03 +0530

    Create 2870. Minimum Number of Operations to Make Array Empty [04.01.24] .java

---
+++ 2870. Minimum Number of Operations to Make Array Empty [04.01.24] .java
@@ -0,0 +1,51 @@
+----------PROBLEM STATEMENT----------
+
+You are given a 0-indexed array nums consisting of positive integers.
+
+There are two types of operations that you can apply on the array any number of times:
+
+Choose two elements with equal values and delete them from the array.
+Choose three elements with equal values and delete them from the array.
+Return the minimum number of operations required to make the array empty, or -1 if it is not possible.
+
+  Constraints:
+
+2 <= nums.length <= 105
+1 <= nums[i] <= 106
+
+----------EXAMPLES----------
+  
+Example 1:
+
+Input: nums = [2,3,3,2,2,4,2,3,4]
+Output: 4
+Explanation: We can apply the following operations to make the array empty:
+- Apply the first operation on the elements at indices 0 and 3. The resulting array is nums = [3,3,2,4,2,3,4].
+- Apply the first operation on the elements at indices 2 and 4. The resulting array is nums = [3,3,4,3,4].
+- Apply the second operation on the elements at indices 0, 1, and 3. The resulting array is nums = [4,4].
+- Apply the first operation on the elements at indices 0 and 1. The resulting array is nums = [].
+It can be shown that we cannot make the array empty in less than 4 operations.
+  
+Example 2:
+
+Input: nums = [2,1,2,2,3,3]
+Output: -1
+Explanation: It is impossible to empty the array.
+
+----------JAVA SOLUTION----------
+
+class Solution {
+    public int minOperations(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            map.put(nums[i] , map.getOrDefault(nums[i],0)+1);
+        }
+        int ans = 0;
+        for(Map.Entry<Integer,Integer> elem : map.entrySet()){
+            int value = elem.getValue();
+            if(value == 1) return -1;
+            ans += Math.ceil((double)value/3);
+        }
+        return ans;
+    }
+}
